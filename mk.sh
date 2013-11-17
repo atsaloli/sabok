ca33011008536cbe40d9bb86c0b9354aaba9675b
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2013-11-17 10:56:24 -0800

    add table of contents

--- mk.sh
+++ mk.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 time a2x -v \
-    -a docinfo2 \
+    -a toc-placement=manual \
+    -a toc2 \
+    -a theme=volnitsky \
     -f xhtml \
     --fop \
-    --xsltproc-opts="--stringparam toc.section.depth 0 --stringparam  section.autolabel 0 --stringparam  generate.toc \"book toc\" " \
+    --xsltproc-opts="--stringparam toc.section.depth 0 --stringparam section.autolabel 0 " \
     -d article \
     sabok.txt | \
   remark ./xmllint.remark && \
   mv sabok.html index.html && \
   google-chrome index.html
+
+#  -a docinfo2 \
