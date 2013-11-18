083139e0cfafaad72faca0fafedf534354940292
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2013-11-17 18:17:48 -0800

    add definition of What is System Administration?

--- mk.sh
+++ mk.sh
@@ -1,6 +1,25 @@
-#!/bin/sh
+#!/bin/sh -x
 
-time a2x -v \
+A2X="a2x -v 
+         -a toc-placement=manual 
+         -a toc2 
+         -a theme=volnitsky 
+         -f xhtml 
+         --fop 
+         -d article "
+
+$A2X sabok.txt | \
+  remark ./xmllint.remark && \
+  mv sabok.html index.html && \
+  google-chrome index.html
+
+$A2X field.txt | \
+  remark ./xmllint.remark && \
+  google-chrome field.html
+exit
+#--xsltproc-opts=\"--stringparam toc.section.depth 0 --stringparam section.autolabel 0 \" 
+
+a2x -v \
     -a toc-placement=manual \
     -a toc2 \
     -a theme=volnitsky \
@@ -12,5 +31,3 @@ time a2x -v \
   remark ./xmllint.remark && \
   mv sabok.html index.html && \
   google-chrome index.html
-
-#  -a docinfo2 \
